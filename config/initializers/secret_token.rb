39bc088daf0e2992a32bb7f8581d82fe4e2ff15c
OpenShift guest <arman@ayvazyan.com>
2013-10-03 14:27:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsLearn::Application.config.secret_token = 'bb4331a421c0a0e6b58e69cdaab275b1888afee9f0863df21e3b679bedca97f3582218f70ad109acfeca297a8e772b7b121fbb3b979e15e27ee813594396dbb8'
